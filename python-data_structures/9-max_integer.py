bad81e151d3d10e5058a28c0c4da875109079617
Yaminadjenkal <mina20101951hotmail.com>
2025-01-22 08:56:39 +0000

    task 8

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Write a function that finds the biggest integer of a list.
+
+Prototype: def max_integer(my_list=[]):
+If the list is empty, return None
+You can assume that the list only contains integers
+You are not allowed to import any module
+You are not allowed to use the builtin max()
+"""
+
+
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    int_max = my_list[0]
+    for numb in my_list:
+        if numb > int_max:
+            int_max = numb
+    return int_max
