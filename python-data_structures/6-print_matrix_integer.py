c6d45f03dfe94d39363a06c3791f43ab182dade7
Yaminadjenkal <mina20101951hotmail.com>
2025-01-22 08:51:41 +0000

    task 7

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
+"""
+Write a function that prints a matrix of integers.
+
+Prototype: def print_matrix_integer(matrix=[[]]):
+Format: see example
+You are not allowed to import any module
+You can assume that the list only contains integers
+You are not allowed to cast integers into strings
+You have to use str.format() to print integers
+"""
+
+
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for i, element in enumerate(row):
-            if i != len(row) - 1:
-                print("{:d}".format(element), end=" ")
-            else:
-                print("{:d}".format(element))
+    for iteri in range(len(matrix)):
+        for iterj in range(len(matrix[iteri])):
+            print("{:d}".format(matrix[iteri][iterj]), end="")
+            if iterj != len(matrix[iteri]) - 1:
+                print(" ", end="")
+        print()
