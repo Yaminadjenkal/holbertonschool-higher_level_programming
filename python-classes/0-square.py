4936c1a12a36f0610c73f0075dbe369266f12557
Yaminadjenkal <mina20101951@hotmail.com>
2025-01-27 14:39:46 +0100

    fix all my code

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+'''
+class that defined a square
+'''
+
+
 class Square:
     """Empty class that defines a square"""
     pass
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,6 +2,6 @@
 class Square:
     '''class that defined a square'''
     def __init__(self,size):
-        '''initilization for the method of the square , this method sets 
-        the size of the square'''
+        """initilization for the method of the square , this method sets 
+        the size of the square"""
         self.size = size
