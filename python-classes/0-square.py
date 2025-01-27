3f5977c6897b98b820689258b021ebaff8a6f25f
Yaminadjenkal <mina20101951@hotmail.com>
2025-01-27 14:14:19 +0100

    fix my code

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 class Square:
-    '''class that defined a square 
-    '''
+    '''Class that defines a square.'''
+    
     def __init__(self):
-        '''initialization method that defined a square , the methode
-        does nothing for the moment by using pass'''
+        '''Initialization method for the Square class.
+        This method does nothing for the moment by using pass.'''
         pass
