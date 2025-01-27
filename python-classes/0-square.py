dd85ce65dcdb2b80d9603442083df5f0a0fcd4fd
Yaminadjenkal <mina20101951@hotmail.com>
2025-01-27 14:05:11 +0100

    my week project

---
+++ python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Square:
+    '''class that defined a square 
+    '''
+    def __init__(self):
+        '''initialization method that defined a square , the methode
+        does nothing for the moment by using pass'''
+        pass
