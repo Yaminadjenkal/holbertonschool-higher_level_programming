b7aa4a382e83b6d46e8a449d48c7364181d630db
Yaminadjenkal <mina20101951@hotmail.com>
2025-01-28 15:18:59 +0000

    fix my code

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-'''
-class that defined a square
-'''
+# 0-square.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Define a class Square."""
 
 
 class Square:
-    
-    """Empty class that defines a square"""
+    """Represent a square."""
     pass
