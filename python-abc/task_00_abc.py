5998bb37ee82d198e17703198823514c68cf3f95
Yaminadjenkal <mina20101951@hotmail.com>
2025-02-05 12:23:44 +0100

    mon projet de la semaine

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+""" import the abc abstract method"""
+
+
+class Animal(ABC):
+    @abstractmethod
+    def sound(self):
+        pass
+
+class Dog(Animal):
+    def sound(self):
+        return "Bark"
+
+class Cat(Animal):
+    def sound(self):
+        return "Meow"
