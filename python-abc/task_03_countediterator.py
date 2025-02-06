bce24e3eb955b8f2ad29eb7227571481d4b5e31f
Yaminadjenkal <mina20101951@hotmail.com>
2025-02-06 14:17:26 +0100

    fix my codeg

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -4,15 +4,15 @@ class CountedIterator:
         self.iterable = iterable
         self.iterator = iter(iterable)
         self.count = 0
-        
-def __iter__(self):
-    return self
 
-def __next__(self):
-    try:
-        item = next(self.iterator)
-        self.count += 1
-        print(f"Iteration {self.count}: {item}")
-        return item
-    except StopIteration:
-        raise StopIteration
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            item = next(self.iterator)
+            self.count += 1
+            print(f"Iteration {self.count}: {item}")
+            return item
+        except StopIteration:
+            raise StopIteration
