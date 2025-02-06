151670c4e5182d6cf2c3d7a38f29cec794c34822
Yaminadjenkal <mina20101951@hotmail.com>
2025-02-06 11:07:19 +0100

    task 2

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,17 @@
+class verboselist(lis):
+    def __init__(self, *args):
+        super().__init__(*args)
+    def append(self, item):
+        print(f"Added [item] to the list.")
+        super().append(item)
+    def extend(self, item):
+        print(f"Extended the list with [x] items.")
+        super().extend(item)
+        print(f"Extended the list with [x] items.")
+        super().extend(item)
+    def remove(self, item):
+        print(f"Removed [item] from the list.")
+        super().remove(item)
+    def pop(self, item):
+        print(f"Popped [item] from the list.")
+        super().pop(item)
