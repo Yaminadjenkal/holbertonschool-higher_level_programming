17a96bedb6bbf313e26ad8c0e6b697977d3f87fc
Yaminadjenkal <mina20101951@hotmail.com>
2025-02-06 14:02:39 +0100

    task 3

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+class CountedIterator:
+    def __init__(self, iterable):
+        self.iterable = iterable
+        self.iterator = iter(iterable)
+        self.count = 0
+        
+def __iter__(self):
+    return self
+
+def __next__(self):
+    try:
+        item = next(self.iterator)
+        self.count += 1
+        print(f"Iteration {self.count}: {item}")
+        return item
+    except StopIteration:
+        raise StopIteration
