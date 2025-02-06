fe5bc784091976da35f9a3790201d1c7bd1f3b6e
Yaminadjenkal <mina20101951@hotmail.com>
2025-02-06 15:02:18 +0100

    task 3

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
-"""class that defined a
-countediterator
-"""
 
-
+# Définition de la classe CountedIterator
 class CountedIterator:
+    # Initialisation de l'itérateur avec l'itérable donné
     def __init__(self, iterable):
-        """initialization of itaration method
-        """
-        self.iterable = iterable
-        self.iterator = iter(iterable)
-        self.count = 0
+        self.iterable = iterable  # Enregistre l'itérable
+        self.iterator = iter(iterable)  # Crée un itérateur à partir de l'itérable
+        self.count = 0  # Initialise le compteur à 0
 
+    # Méthode spéciale pour retourner l'itérateur lui-même
     def __iter__(self):
-        """initialization of the iteration methode
-        """
         return self
 
+    # Méthode spéciale pour obtenir l'élément suivant de l'itérateur
     def __next__(self):
-        """defined the methode to next to the element """
         try:
+            # Obtient le prochain élément de l'itérateur
             item = next(self.iterator)
-            self.count += 1
-            print(f"Iteration {self.count}: {item}")
-            return item
+            self.count += 1  # Incrémente le compteur
+            print(f"Iteration {self.count}: {item}")  # Affiche le compteur et l'élément
+            return item  # Retourne l'élément
         except StopIteration:
+            # Si l'itérateur est épuisé, lève l'exception StopIteration
             raise StopIteration
+
+# Exemple d'utilisation
+if __name__ == "__main__":
+    items = [1, 2, 3, 4, 5]  # Liste d'éléments
+    counted_iter = CountedIterator(items)  # Crée une instance de CountedIterator
+
+    # Itère sur les éléments de counted_iter
+    for item in counted_iter:
+        pass  # Pass signifie qu'on ne fait rien de spécial avec les éléments dans cet exemple
