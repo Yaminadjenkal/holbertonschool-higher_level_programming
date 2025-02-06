3a39dd3f2f2d28b75db0f0d366dc44a794a00d87
Yaminadjenkal <mina20101951@hotmail.com>
2025-02-06 14:22:36 +0100

    fix my code

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
+"""class that defined a
+countediterator
+"""
+
+
 class CountedIterator:
     def __init__(self, iterable):
+        """initialization of itaration method
+        """
         self.iterable = iterable
         self.iterator = iter(iterable)
         self.count = 0
 
     def __iter__(self):
+        """initialization of the iteration methode
+        """
         return self
 
     def __next__(self):
+        """defined the methode to next to the element """
         try:
             item = next(self.iterator)
             self.count += 1
