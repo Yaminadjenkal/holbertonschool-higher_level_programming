53a0424dca2d2602a2030f80293d76ad45d50d86
Yaminadjenkal <mina20101951@hotmail.com>
2025-02-11 20:12:52 +0100

    fix

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""defines function to read text file and prints to stdout"""
+
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="utf-8") as f:
-        print(f.read())
+    """reads text file and prints to stdout"""
+    with open(filename) as f:
+        print(f.read(), end="")
