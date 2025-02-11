5afb26c6e0cff9d2123ad502b073784bee2f6e95
Yaminadjenkal <mina20101951@hotmail.com>
2025-02-11 20:21:34 +0100

    commit

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,21 +1,8 @@
 #!/usr/bin/python3
+"""defines function to write string to text file"""
+
 
 def write_file(filename="", text=""):
-    """
-    Écrit une chaîne de caractères dans un fichier texte (UTF8) et renvoie le nombre de caractères écrits.
-
-    :param filename: Nom du fichier
-    :param text: Texte à écrire dans le fichier
-    :return: Nombre de caractères écrits
-    """
-    # Utilisation de l'instruction 'with' pour ouvrir le fichier en mode écriture
-    with open(filename, "w", encoding="utf-8") as f:
-        # Écrire le texte dans le fichier et retourner le nombre de caractères écrits
+    """writes string to text file"""
+    with open(filename, 'w') as f:
         return f.write(text)
-
-# Exemple d'utilisation de la fonction
-if __name__ == "__main__":
-    # Appel de la fonction write_file avec un nom de fichier et un texte
-    nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
-    # Affichage du nombre de caractères écrits
-    print(nb_characters)
