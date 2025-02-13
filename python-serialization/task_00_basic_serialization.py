7b58e155cd7ce0c2e107bc8bca13592890c54ab6
Yaminadjenkal <mina20101951@hotmail.com>
2025-02-13 13:08:38 +0100

    fix my code

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 import json
 
-def serialize_and_save_to_file(date,filename):
+def serialize_and_save_to_file(data, filename):
     """
     Sérialiser un dictionnaire Python et l'enregistrer dans un fichier JSON.
     
     :param data: Un dictionnaire Python contenant les données
     :param filename: Le nom du fichier de sortie JSON. Si le fichier existe déjà, il sera remplacé.
     """
-    with open('data.json','w', encoding='utf-8') as f:
+    with open(filename, "w", encoding="utf-8") as f:
         json.dump(data, f)
-        
+
 def load_and_deserialize(filename):
     """
     Charger et désérialiser un fichier JSON pour recréer un dictionnaire Python.
@@ -20,3 +20,4 @@ def load_and_deserialize(filename):
     """
     with open(filename, "r", encoding="utf-8") as f:
         return json.load(f)
+    
