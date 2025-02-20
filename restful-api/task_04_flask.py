298017dbc151eaf907cc101f8b179d5fbbe6bd68
Yaminadjenkal <mina20101951@hotmail.com>
2025-02-20 10:51:58 +0100

    fix

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -20,20 +20,28 @@ users = {
     }
 }
 
+
+ERROR_USER_NOT_FOUND = {"error": "User not found"}
+ERROR_USERNAME_REQUIRED = {"error": "Username is required"}
+
+
 # Route de l'accueil
 @app.route("/")
 def home():
     return "Welcome to the Flask API!"
+
 
 # Route de statut
 @app.route("/status")
 def status():
     return "OK"
 
+
 # Route pour obtenir tous les noms d'utilisateurs
 @app.route("/data")
 def get_usernames():
     return jsonify(list(users.keys()))
+
 
 # Route pour obtenir les détails d'un utilisateur spécifique
 @app.route("/users/<username>")
@@ -41,7 +49,8 @@ def get_user(username):
     user = users.get(username)
     if user:
         return jsonify(user)
-    return jsonify({"error": "User not found"}), 404
+    return jsonify(ERROR_USER_NOT_FOUND), 404
+
 
 # Route pour ajouter un nouvel utilisateur
 @app.route("/add_user", methods=["POST"])
@@ -50,10 +59,11 @@ def add_user():
     username = data.get("username")
 
     if not username:
-        return jsonify({"error": "Username is required"}), 400
+        return jsonify(ERROR_USERNAME_REQUIRED), 400
 
     users[username] = data
     return jsonify({"message": "User added", "user": data}), 201
+
 
 # Lancement de l'application Flask
 if __name__ == "__main__":
