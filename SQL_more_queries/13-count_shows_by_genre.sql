47b8baaf7b85a1815d4be1eaee01512b13102f0b
Yaminadjenkal <mina20101951@hotmail.com>
2025-03-06 10:10:08 +0000

    all the tasks

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- List all genres with the count of shows linked to each genre
+SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM genres
+INNER JOIN tv_show_genres
+ON genres.id = tv_show_genres.genre_id
+GROUP BY genres.id
+HAVING COUNT(tv_show_genres.show_id) > 0
+ORDER BY number_of_shows DESC;
+
---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- List all shows and their genres, with NULL for shows without genres
+SELECT tv_shows.title, genres.name
+FROM tv_shows
+LEFT OUTER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+LEFT OUTER JOIN genres
+ON tv_show_genres.genre_id = genres.id
+ORDER BY tv_shows.title ASC, genres.name ASC;
+
