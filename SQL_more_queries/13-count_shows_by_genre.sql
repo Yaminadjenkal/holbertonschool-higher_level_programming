23e6a95936c551bd0b814f4068cd2831f1f6d3a3
Yaminadjenkal <mina20101951@hotmail.com>
2025-03-06 11:28:33 +0000

    all the tasks

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,7 +3,6 @@ SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM genres
 INNER JOIN tv_show_genres
 ON genres.id = tv_show_genres.genre_id
-GROUP BY genres.id
-HAVING COUNT(tv_show_genres.show_id) > 0
+GROUP BY genres.name
 ORDER BY number_of_shows DESC;
 
