a691629340b35335aa95b277270f0a0c26efa3db
Yaminadjenkal <mina20101951@hotmail.com>
2025-03-06 11:55:17 +0000

    all the tasks

--- SQL_more_queries/hbtn_0d_tvshows.sql
+++ SQL_more_queries/hbtn_0d_tvshows.sql
@@ -1,104 +1,79 @@
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_tvshows;
+
+-- Use the created database
 USE hbtn_0d_tvshows;
--- MySQL dump 10.13  Distrib 5.7.8-rc, for Linux (x86_64)
---
--- Host: localhost    Database: hbtn_0d_tvshows
--- ------------------------------------------------------
--- Server version	5.7.8-rc
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- Disable foreign key checks to avoid constraints issues while dropping tables
+SET FOREIGN_KEY_CHECKS = 0;
 
---
--- Table structure for table `tv_genres`
---
+-- Drop existing tables if they exist
+DROP TABLE IF EXISTS tv_show_genres;
+DROP TABLE IF EXISTS tv_genres;
+DROP TABLE IF EXISTS tv_shows;
 
-DROP TABLE IF EXISTS `tv_genres`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tv_genres` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(256) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- Enable foreign key checks back
+SET FOREIGN_KEY_CHECKS = 1;
 
---
--- Dumping data for table `tv_genres`
---
+-- Create the tv_genres table
+CREATE TABLE tv_genres (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(256) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB;
 
-LOCK TABLES `tv_genres` WRITE;
-/*!40000 ALTER TABLE `tv_genres` DISABLE KEYS */;
-INSERT INTO `tv_genres` VALUES (1,'Drama'),(2,'Mystery'),(3,'Adventure'),(4,'Fantasy'),(5,'Comedy'),(6,'Crime'),(7,'Suspense'),(8,'Thriller');
-/*!40000 ALTER TABLE `tv_genres` ENABLE KEYS */;
-UNLOCK TABLES;
+-- Insert data into tv_genres
+INSERT INTO tv_genres (id, name)
+VALUES 
+(1, 'Drama'),
+(2, 'Mystery'),
+(3, 'Adventure'),
+(4, 'Fantasy'),
+(5, 'Comedy'),
+(6, 'Crime'),
+(7, 'Suspense'),
+(8, 'Thriller');
 
---
--- Table structure for table `tv_show_genres`
---
+-- Create the tv_shows table
+CREATE TABLE tv_shows (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(256) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB;
 
-DROP TABLE IF EXISTS `tv_show_genres`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tv_show_genres` (
-  `show_id` int(11) NOT NULL,
-  `genre_id` int(11) NOT NULL,
-  KEY `show_id` (`show_id`),
-  KEY `genre_id` (`genre_id`),
-  CONSTRAINT `tv_show_genres_ibfk_1` FOREIGN KEY (`show_id`) REFERENCES `tv_shows` (`id`),
-  CONSTRAINT `tv_show_genres_ibfk_2` FOREIGN KEY (`genre_id`) REFERENCES `tv_genres` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- Insert data into tv_shows
+INSERT INTO tv_shows (id, title)
+VALUES 
+(1, 'House'),
+(2, 'Game of Thrones'),
+(3, 'The Big Bang Theory'),
+(4, 'New Girl'),
+(5, 'Silicon Valley'),
+(6, 'Breaking Bad'),
+(7, 'Better Call Saul'),
+(8, 'Dexter'),
+(9, 'Homeland'),
+(10, 'The Last Man on Earth');
 
---
--- Dumping data for table `tv_show_genres`
---
+-- Create the tv_show_genres table
+CREATE TABLE tv_show_genres (
+  show_id INT NOT NULL,
+  genre_id INT NOT NULL,
+  KEY show_id (show_id),
+  KEY genre_id (genre_id),
+  CONSTRAINT tv_show_genres_ibfk_1 FOREIGN KEY (show_id) REFERENCES tv_shows (id),
+  CONSTRAINT tv_show_genres_ibfk_2 FOREIGN KEY (genre_id) REFERENCES tv_genres (id)
+) ENGINE=InnoDB;
 
-LOCK TABLES `tv_show_genres` WRITE;
-/*!40000 ALTER TABLE `tv_show_genres` DISABLE KEYS */;
-INSERT INTO `tv_show_genres` VALUES (1,1),(1,2),(2,3),(2,1),(2,4),(3,5),(4,5),(5,5),(6,6),(6,1),(6,7),(6,8),(8,6),(8,1),(8,2),(8,7),(8,8),(10,5),(10,1);
-/*!40000 ALTER TABLE `tv_show_genres` ENABLE KEYS */;
-UNLOCK TABLES;
+-- Insert data into tv_show_genres
+INSERT INTO tv_show_genres (show_id, genre_id)
+VALUES 
+(1, 1), (1, 2),
+(2, 3), (2, 1), (2, 4),
+(3, 5),
+(4, 5),
+(5, 5),
+(6, 6), (6, 1), (6, 7), (6, 8),
+(8, 6), (8, 1), (8, 2), (8, 7), (8, 8),
+(10, 5), (10, 1);
 
---
--- Table structure for table `tv_shows`
---
-
-DROP TABLE IF EXISTS `tv_shows`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tv_shows` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(256) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tv_shows`
---
-
-LOCK TABLES `tv_shows` WRITE;
-/*!40000 ALTER TABLE `tv_shows` DISABLE KEYS */;
-INSERT INTO `tv_shows` VALUES (1,'House'),(2,'Game of Thrones'),(3,'The Big Bang Theory'),(4,'New Girl'),(5,'Silicon Valley'),(6,'Breaking Bad'),(7,'Better Call Saul'),(8,'Dexter'),(9,'Homeland'),(10,'The Last Man on Earth');
-/*!40000 ALTER TABLE `tv_shows` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2017-02-26 20:57:02
